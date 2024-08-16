51db80af2e4649e766dfb32c8a7dbd7fa137b682
Ashraf50 <ashrafesam00@gmail.com>
2024-08-16 12:38:49 +0300

    change map  style to night mood

--- lib/view/google_map_view.dart
+++ lib/view/google_map_view.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMapView extends StatelessWidget {
+class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
 
   @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: GoogleMap(
-          initialCameraPosition: CameraPosition(target: LatLng(100, 40))),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            markers: markers,
+            onMapCreated: (controller) {
+              googleMapController = controller;
+              initMapStyle();
+            },
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(31.1, 30.5),
+              zoom: 10,
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            right: 130,
+            child: ElevatedButton(
+              onPressed: () {
+                googleMapController.animateCamera(
+                  CameraUpdate.newLatLng(
+                    const LatLng(31.19992823449557, 29.919197953782454),
+                  ),
+                );
+              },
+              child: const Text("Change Location"),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void initMapStyle() async {
+    var assetsMapStyle = await DefaultAssetBundle.of(context)
+        .loadString("assets/map_styles/night_map_style.json");
+    googleMapController.setMapStyle(assetsMapStyle);
   }
 }
